0c27e63d97f5b3b8219a823993fbe64e0d91d0e0
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2021-01-11 22:20:11 -0500

    [add]: Set new API parameters into BU dash format

--- src/pages/buDash.js
+++ src/pages/buDash.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-//Page Component
+
 const Budash = () => {
     const [resp, setResp] = useState([])
     const [project, setProject] = useState('')
+    const [chars, setChar] = useState([])
+    const [info, setInfo] = useState()
+
     useEffect(() => {
         const getAPI = async () => {
             const { data } = await axios.get('https://rickandmortyapi.com/api/location/3,20,50,65')
@@ -15,7 +18,28 @@ const Budash = () => {
     const handleClick = event => {
         setProject(event.target.outerText)
     }
+    const handleClick2 = (char) => {
+        setInfo(char)
+    }
     let [ task ] = project ? resp.filter(item => item.name.includes(project)): '';
+    useEffect(() => {
+        if (!task) return
+        setInfo()
+        if (task.residents.length === 0) {
+            setChar([])
+            return
+        }
+        let resident_list=[]
+        for (let count =0; count < task.residents.length && count < 5; count++) {
+            const res_num = task.residents[count].replace('https://rickandmortyapi.com/api/character/','')
+            resident_list.push(res_num);
+        }
+        const getAPI = async () => {
+            const { data } = await axios.get(`https://rickandmortyapi.com/api/character/${resident_list.join(',')}`)
+            setChar(data)
+        }
+        getAPI()
+    }, [task])
     return (
         <div className='body-container'>
             <div className='m-2'>
@@ -37,35 +61,44 @@ const Budash = () => {
                     <div className='flex flex-row '>
                         <div className= 'flex flex-col bg-gray-50 rounded-2xl w-1/2'>
                             <div className='m-2 text-2xl flex flex-row'>
-                                <svg className="w-6 h-6 m-1" fill="none" stroke="currentColor"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                                ><path strokeLinecap="round" strokeLinejoin="round" 
-                                strokeWidth={2} d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                                /></svg>
+                                <svg className="w-6 h-6 m-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
                                 {task ? task.name : ''}
                             </div>
                             <h2 className='m-2'>{task ? task.type : ''}</h2>
-                            <p className='m-2'>{task ? task.created : ''}</p>
+                            <p className='m-2'>{task ? 
+                            <>
+                            {
+                                new Date(task.created).toLocaleString('en-US')
+                            }
+                            </> : ''}</p>
                             <h2 className='text-gray-500 m-2'>Objectives</h2>
-                            <div className='proy-item'>{task ? task.residents[0] : ''}</div>
-                            <div className='proy-item'>{task ? task.residents[1] : ''}</div>
-                            <div className='proy-item'>{task ? task.residents[2] : ''}</div>
-                            <div className='proy-item'>{task ? task.residents[3] : ''}</div>
-                            <div className='proy-item'>{task ? task.residents[4] : ''}</div>
+                            {
+                                chars.length === 0 ? <div className='proy-item'>No People in here</div> :
+                                    chars.map(char => <div className='proy-item' onClick={() => handleClick2(char)} key={char.name + char.id}>{char.name}</div>)
+                            }
                         </div>
                         <div className= 'flex flex-col m-2 w-1/2'>
                             <p className='py-3'>Objectives Milestone Resume</p>
-                            <p className= 'bg-gray-50 rounded-2xl py-3 m-2'>item 1</p>
-                            <p className= 'bg-gray-50 rounded-2xl py-3 m-2'>item 2</p>
-                            <p className= 'bg-gray-50 rounded-2xl py-3 m-2'>item 3</p>
-                            <p className= 'bg-gray-50 rounded-2xl py-3 m-2'>item 4</p>
-                            <p className= 'bg-gray-50 rounded-2xl py-3 m-2'>item 5</p>
+                            {!info ? 'seleccione personaje': 
+                            <>
+                                <img src={info.image} alt={info.name}/>
+                            </>
+                            }
                         </div>
                     </div>
                     <div className='flex flex-row'>
-                        <p className= 'bg-gray-50 rounded-2xl m-2 w-1/3'>slot 1</p>
-                        <p className= 'bg-gray-50 rounded-2xl m-2 w-1/3'>slot 2</p>
-                        <p className= 'bg-gray-50 rounded-2xl m-2 w-1/3'>slot 3</p>
+                        {!info ? 
+                        <>
+                            <p className= 'bg-gray-50 rounded-2xl m-2 w-1/3'>Gender</p>
+                            <p className= 'bg-gray-50 rounded-2xl m-2 w-1/3'>Species</p>
+                            <p className= 'bg-gray-50 rounded-2xl m-2 w-1/3'>status</p> 
+                        </>: 
+                            <>
+                                <p className= 'bg-gray-50 rounded-2xl m-2 w-1/3'>Gender: {info.gender}</p>
+                                <p className= 'bg-gray-50 rounded-2xl m-2 w-1/3'>Species: {info.species}</p>
+                                <p className= 'bg-gray-50 rounded-2xl m-2 w-1/3'>Status: {info.status}</p> 
+                            </>
+                        }
                     </div>
                 </div>
             </div>
